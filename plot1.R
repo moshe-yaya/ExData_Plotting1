6ac76025f208078dc3aafabe1d55d235c766a050
moshe-yaya <moshe.yaya@gmail.com>
2014-08-10 19:57:49 +0300

    Course Project 1
    Exploratory Data Analysis

---
+++ plot1.R
@@ -0,0 +1,23 @@
+Sys.setlocale("LC_TIME", "English")
+temp     <- tempfile()
+fileName <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+coll <- c('Date','Time','Global_active_power',
+          'Global_reactive_power','Voltage','Global_intensity',
+          'Sub_metering_1','Sub_metering_2','Sub_metering_3')    
+download.file(fileName,temp)
+data     <- read.table(unz(temp, "household_power_consumption.txt"),
+                       header = TRUE, sep=";", as.is = TRUE, stringsAsFactors = FALSE,
+                       na.strings = "?", skip = 66636 , nrows = 2880, col.names = coll
+)
+unlink(temp)
+DateTime <-strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S") 
+data <- cbind(DateTime,data)
+##----------------------------------------
+
+
+hist(data$Global_active_power, col ='red',
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",
+     ylim = c(0,1200)      )
+
+dev.copy(png, file = "plot1.png")
